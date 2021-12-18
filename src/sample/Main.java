9514a5204a88698fbbeeccc5957aafc97758376b
KonstantinosKol <dit18090@go.uop.gr>
2021-12-18 20:20:27 +0200

    Multi-fields(<tags>), Boolean, Phases("text") query support.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,19 +6,31 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
-        primaryStage.setTitle("Articles Search Engine");
+        primaryStage.setTitle("TReSA");
         primaryStage.setScene(new Scene(root, 1215, 800));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        LuceneTester.main(args);
+
+        //Creating the index when the program starts
+        LuceneTester tester;
+        try {
+            tester = new LuceneTester();
+            tester.createIndex();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
         launch(args);
     }
 }
